cf62a7b9664e28dcddbb657b60c7cdf793458991
miguel angel <mg.aangel10@gmail.com>
2023-01-13 13:04:20 +0100

    nuevo proyecto java

---
+++ src/holamun/Principal.java
@@ -0,0 +1,13 @@
+package holamun;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("holaaaaaaaa");
+
+	}
+
+}
